689e4fd132dc0cadd86dad11c21ee352ece24fb7
Matheus Souza <msouza6192@gmail.com>
2024-01-22 18:51:20 -0500

    RecipeCard start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import RecipeCard from "./components/RecipeCard";
+
+
+const recipeList = [
+  {
+  id: 0,
+  name: "Homemade Ramen",
+  rating: null,
+  category: "Asian"
+  },
+  {
+    id: 1,
+    name: "Feijoada",
+    rating: null,
+    category: "Latin"
+  },
+  {
+    id: 3,
+    name: "Tuscan Chicken",
+    rating: null,
+    category: "Italian"
+  },
+  {
+    id: 4,
+    name: "BBQ Ribs",
+    rating: null,
+    category: "American"
+  },
+  {
+    id: 5,
+    name: "Beef Stew",
+    rating: null,
+    category: "Latin"
+  },
+
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Recipe App</h1>
+      { recipeList.map( recipe1 => <RecipeCard recipe={recipe1}/> )}
     </div>
   );
 }
